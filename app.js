d149c1583269846d4f094f26c092727b3bb05890
jvmMachado <joaovmmachado@gmail.com>
2020-02-23 02:01:30 -0300

    add chained route handlers and level 1 authentication

--- app.js
+++ app.js
@@ -2,12 +2,84 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
  
 const app = express();
  
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Please enter an email.']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please enter a password.']
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.route('/login')
+
+    .get( (req, res) => {
+    res.render('login');
+})
+
+    .post( (req, res) => {
+
+        const username = req.body.username
+        const password = req.body.password
+
+        User.findOne({ email: username }, (err, foundUser) => {
+            if (err) {
+                console.log(err);
+            } else {
+                if (foundUser) {
+                    if (foundUser.password === password) {
+                        res.render('secrets');
+                    } else {
+                        console.log('Wrong password!!');
+                    }
+                } else {
+                    console.log('User not found!!');
+                }
+            }
+        });
+
+});
+
+app.route('/register')
+
+    .get( (req, res) => {
+    res.render('register');
+})
+
+    .post( (req, res) => {
+        
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+
+        newUser.save(err => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets');
+            }
+        });
+
+    })
  
 app.listen(3000, () => {
     console.log('Server started on port 3000.');
